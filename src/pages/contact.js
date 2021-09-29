ea6289da8876fbf108bd2a3bbacc2cfd2bcbbc1d
Zaklaria <zsumonn@gmail.com>
2021-09-29 20:20:52 +0600

    added formpree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,7 +24,11 @@ const Contact = ({ data }) => {
             </p>
           </article>
           <article>
-            <form className="form contact-form">
+            <form
+              className="form contact-form"
+              action="https://formspree.io/f/xdoyjkwa"
+              method="POST"
+            >
               <div className="form-row">
                 <label htmlFor="name">your name</label>
                 <input type="text" name="name" id="name" />
